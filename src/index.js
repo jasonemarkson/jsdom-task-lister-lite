cea0ba568b32c4de2d793328f2349a01e8b4a5b0
jasonemarkson <jasonmarkson76@gmail.com>
2021-05-22 11:48:51 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+const newTaskDescription = document.getElementById('new-task-description')
+const newTaskForm = document.getElementById('create-task-form')
+const taskList = document.getElementById('tasks')
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  newTaskForm.addEventListener('submit', addTaskToList)
+
 });
+
+function addTaskToList(e) {
+  e.preventDefault()
+  taskList.innerHTML += `<li> ${newTaskDescription.value} <button data-description=${newTaskDescription.value}>Delete</button></li>`
+  newTaskDescription.value = ""
+}
